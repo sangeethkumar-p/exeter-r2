a16ee80af8ac014667ed7ca54927002a655efbee
sangeethkumar-p <77340877+sangeethkumar-p@users.noreply.github.com>
2021-01-13 08:31:26 +0530

    Add files via upload

---
+++ code.java
@@ -0,0 +1,33 @@
+import java.util.*; 
+import java.nio.charset.StandardCharsets; 
+import java.nio.file.*; 
+import java.io.*; 
+public class ReadFileIntoList 
+{ 
+public static List<String> readFileInList(String fileName) 
+{ 
+
+	List<String> lines = Collections.emptyList(); 
+	try
+	{ 
+	lines = 
+	Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8); 
+	} 
+
+	catch (IOException e) 
+	{ 
+
+	// do something 
+	e.printStackTrace(); 
+	} 
+	return lines; 
+} 
+public static void main(String[] args) 
+{ 
+	List l = readFileInList("C:\\Users\\pankaj\\Desktop\\test.java"); 
+
+	Iterator<String> itr = l.iterator(); 
+	while (itr.hasNext()) 
+	System.out.println(itr.next()); 
+} 
+}
